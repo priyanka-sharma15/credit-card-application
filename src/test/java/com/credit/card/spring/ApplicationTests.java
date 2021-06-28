ce2693bbae1610db1119a63e521c0a6ceb73aaf2
priyanka-sharma15 <mailtosharmap2@gmail.com>
2021-06-28 05:49:57 +0100

    Update test cases

--- src/test/java/com/credit/card/spring/ApplicationTests.java
+++ src/test/java/com/credit/card/spring/ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.credit.card.spring;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
@@ -7,31 +8,40 @@ import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import com.credit.card.spring.domain.CreditCard;
 import com.credit.card.spring.repository.CardsRepository;
+import com.credit.card.spring.util.Luhn10;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = CreditCardApplication.class)
+@SpringBootTest(classes = ApplicationTests.class)
+
 public class ApplicationTests {
-	
+
 	@Autowired
 	private CardsRepository cardsRepo;
 
+	@Autowired
+	private Luhn10 algo;
+
 	@Test
-	public void testCreditCard(){
-		CreditCard cerditCardObj = new CreditCard("test", 0, "2000", "2345676543");
+	public void testCreditCard() {
+		CreditCard cerditCardObj = new CreditCard("test", 0, "2000", "79927398713");
 		CreditCard newCard = cardsRepo.save(cerditCardObj);
 		assertNotNull(newCard);
 		assertNotNull(newCard.getCcNumber());
-		
 		List<CreditCard> allCards = cardsRepo.findAll();
 		assertNotNull(allCards);
 		assertFalse(allCards.isEmpty());
-		
 	}
 
+	@Test
+	public void testCreditCard_invalid(){
+		CreditCard cerditCardObj = new CreditCard("test", 0, "2000", "79927398712");
+		boolean ccNumber = algo.Check(cerditCardObj.getCcNumber());
+		assertEquals(false, ccNumber);
+	}
 }
